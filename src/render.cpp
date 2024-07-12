e64ef9d7c6a2034be2bd7bdb8f4e54dcd11c1656
sayfiddinzoda <assamarqandiy2005@gmail.com>
2024-07-12 17:18:00 +0500

    feat: implementing random function for top & left

--- src/render.cpp
+++ src/render.cpp
@@ -1,5 +1,6 @@
 #include "../include/render.h"
-
+#include <ctime>
+#include <cstdlib>
 int *defineSize() {
   int *arr = new int[2];
   arr[0] = 80; // width
@@ -12,8 +13,14 @@ void render(int top, int left, std::string content) {
   int *windows_size = defineSize();
   int width = windows_size[0];
   int height = windows_size[1];
-
-  // code goes here
-
+  string txt;
+  srand(time(NULL));
+  int top = rand()%height+1;
+  srand(time(NULL));
+  int left = rand()%weight+1;
+  for (int i=0; i<top; i++) cout << "#\n";
+  for (int i=0; i<left-(txt.size())*(weight<left+txt.size()); i++) cout << "#";
+  cout << txt;
+  for (int i=0; i<height-top-1; i++) cout << "#\n";
   delete[] windows_size;
 }
